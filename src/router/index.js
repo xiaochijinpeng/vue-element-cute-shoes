580983e917814041fee6d48493d883ce87537104
cutiechi <chi@cutie.ooo>
2018-09-05 14:10:03 +0800

    feat(page): add home page

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+import Home from '../pages/Home'
+
 Vue.use(Router)
 
-export default new Router({
-  routes: []
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  }
+]
+
+const router = new Router({
+  routes
 })
+
+export default router
