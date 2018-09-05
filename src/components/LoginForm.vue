dc4d3206efb348e51a7916891cb3fa2b80f16c26
cutiechi <chi@cutie.ooo>
2018-09-05 16:10:12 +0800

    feat(component): add login form component

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,76 @@
+<template>
+  <el-form ref="loginForm" :model="loginForm" :rules="loginFormRules">
+    <p>Cute Shoes</p>
+    <el-form-item prop="userName">
+      <el-input v-model="loginForm.userName" placeholder="请输入用户名称"></el-input>
+    </el-form-item>
+    <el-form-item prop="userPassword">
+      <el-input type="password" v-model="loginForm.userPassword" placeholder="请输入用户密码">
+        <el-button type="info" slot="append">忘记密码</el-button>
+      </el-input>
+    </el-form-item>
+    <el-button type="primary" @click="login">登录</el-button>
+  </el-form>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      loginForm: {
+        userName: '',
+        userPassword: ''
+      },
+      loginFormRules: {
+        userName: [
+          {
+            required: true,
+            message: '用户名称不能为空',
+            trigger: 'blur'
+          }
+        ],
+        userPassword: [
+          {
+            required: true,
+            message: '用户密码不能为空',
+            trigger: 'blur'
+          }
+        ]
+      }
+    }
+  },
+  methods: {
+    login () {
+      this.$refs.loginForm.validate(valid => {
+        if (valid) {
+
+        } else {
+          return false
+        }
+      })
+    },
+    forgetPassword () {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css?family=Monoton');
+
+.el-form {
+  p {
+    color: #9c27b0;
+    font-family: 'Monoton', cursive;
+    text-align: center;
+    font-size: 32px;
+    line-height: 40px;
+    height: 40px;
+  }
+
+  .el-button {
+    width: 100%;
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -4,13 +4,27 @@ import router from './router'
 
 import 'element-ui/lib/theme-chalk/index.css'
 
-import { Header, Menu, MenuItem, Container, Main } from 'element-ui'
+import {
+  Header,
+  Menu,
+  MenuItem,
+  Container,
+  Main,
+  Form,
+  FormItem,
+  Input,
+  Button
+} from 'element-ui'
 
 Vue.use(Header)
 Vue.use(Menu)
 Vue.use(MenuItem)
 Vue.use(Container)
 Vue.use(Main)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.use(Button)
 
 Vue.config.productionTip = false
 
